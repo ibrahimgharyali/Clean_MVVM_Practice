1800788429698989d34da82caf5ce45d28234488
Ibrahim Gharyali <ibrahimgharyali@hotmail.com>
2026-07-26 00:40:04 +0530

    Add application class and setup Compose theme
    - Create `MyPracticeApplication` class and annotate it with `@HiltAndroidApp` for Hilt dependency injection.
    - Implement Material3 color tokens in `Color.kt` for light and dark themes.
    - Define default typography styles in `Type.kt`.
    - Implement `MyPracticeApplicationTheme` in `Theme.kt` with support for dynamic color schemes and dark mode toggling.

---
+++ app/src/main/java/com/ibrahimgharyali/mypracticeapplication/MyPracticeApplication.kt
@@ -0,0 +1,7 @@
+package com.ibrahimgharyali.mypracticeapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyPracticeApplication : Application()
