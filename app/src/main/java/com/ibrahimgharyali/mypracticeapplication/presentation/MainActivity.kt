070019e5a1cc9c393e03d0875d260e87665dc19c
Ibrahim Gharyali <ibrahimgharyali@hotmail.com>
2026-07-26 14:59:47 +0530

    Delete default `MainActivity.kt`
    - Remove the auto-generated `MainActivity` class and its associated `Greeting` Composable and Preview.

--- app/src/main/java/com/ibrahimgharyali/mypracticeapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/ibrahimgharyali/mypracticeapplication/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ibrahimgharyali.mypracticeapplication
+package com.ibrahimgharyali.mypracticeapplication.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.ibrahimgharyali.mypracticeapplication.ui.theme.MyPracticeApplicationTheme
+import com.ibrahimgharyali.mypracticeapplication.presentation.theme.MyPracticeApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
